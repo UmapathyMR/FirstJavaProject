696a772c5212fa7b377ea88a61478f319eff74e5
Umapathy M R <94650712+UmapathyMR@users.noreply.github.com>
2021-12-09 13:18:34 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int [] a = {10,20,30,40};
+
+        for(int i=0;i<a.length;i++)
+
+        {
+
+            System.out.println(a[i]);
+        }
+    }
+}
